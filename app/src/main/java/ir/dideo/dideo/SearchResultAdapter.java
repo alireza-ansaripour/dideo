86039f93e07246db9c6097be89b2d90ada7938ff
Navid Ahmadian <navid.ahmadian@gmail.com>
2016-07-12 22:29:37 +0430

    Change thumbnail provider to UrlImageViewHelper

--- app/src/main/java/ir/dideo/dideo/SearchResultAdapter.java
+++ app/src/main/java/ir/dideo/dideo/SearchResultAdapter.java
@@ -15,6 +15,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.toolbox.ImageLoader;
+import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -68,10 +69,7 @@ public class SearchResultAdapter extends BaseAdapter {
         TextView textView = (TextView)convertView.findViewById(R.id.resultText);
         ImageView imageView = (ImageView) convertView.findViewById(R.id.resultImage);
         Video item = (Video) getItem(position);
-//        String pathToFile = "http://inducesmile.com/wp-content/uploads/2015/03/mobile.jpg";
-        DownloadImageWithURLTask downloadTask = new DownloadImageWithURLTask(imageView);
-        downloadTask.execute(item.thumbnail);
-//        downloadTask.execute(pathToFile);
+        UrlImageViewHelper.setUrlDrawable(imageView, item.thumbnail);
         textView.setText(item.title);
         return convertView;
     }
@@ -80,26 +78,4 @@ class ViewHolder{
     TextView textView;
     ImageView imageView;
     WebView webView;
-}
-class DownloadImageWithURLTask extends AsyncTask<String, Void, Bitmap> {
-    ImageView bmImage;
-    public DownloadImageWithURLTask(ImageView bmImage) {
-        this.bmImage = bmImage;
-    }
-
-    protected Bitmap doInBackground(String... urls) {
-        String pathToFile = urls[0];
-        Bitmap bitmap = null;
-        try {
-            InputStream in = new java.net.URL(pathToFile).openStream();
-            bitmap = BitmapFactory.decodeStream(in);
-        } catch (Exception e) {
-            Log.e("Error", e.getMessage());
-            e.printStackTrace();
-        }
-        return bitmap;
-    }
-    protected void onPostExecute(Bitmap result) {
-        bmImage.setImageBitmap(result);
-    }
 }
